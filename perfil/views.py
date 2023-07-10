1e2ed16729520321e0e6aa0a29747e6f4b8608b8
Anderson Guassu <andersonguassu@hotmail.com>
2023-07-09 22:27:47 -0300

    finalizando!!

--- perfil/views.py
+++ perfil/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import render, redirect
 from .models import Conta, Categoria
 from django.contrib import messages
 from django.contrib.messages import constants
-from .utils import calcula_total
+from .utils import calcula_total, calcula_equilibrio_financeiro
 from extrato.models import Valores
 from django.db.models import Sum
+from datetime import datetime
 
 
 def home(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    entradas = valores.filter(tipo='E')
+    saidas = valores.filter(tipo='S')
+    total_entradas = calcula_total(entradas, 'valor')
+    total_saidas = calcula_total(saidas, 'valor')
     contas = Conta.objects.all()
     saldo_total = calcula_total(contas, 'valor')
+
+    percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro() # noqa
     return render(request, 'home.html',
-                  {'contas': contas, 'saldo_total': saldo_total})
+                  {'contas': contas,
+                   'saldo_total': saldo_total,
+                   'total_entradas': total_entradas,
+                   'total_saidas': total_saidas,
+                   'percentual_gastos_essenciais':
+                   int(percentual_gastos_essenciais),
+                   'percentual_gastos_nao_essenciais':
+                   int(percentual_gastos_nao_essenciais),
+                   })
 
 
 def gerenciar(request):
